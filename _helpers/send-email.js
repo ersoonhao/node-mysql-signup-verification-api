3c6f7fa94c3298019226c712c1bfa9b0300ff4b1
ersoonhao <soonhao.er@gmail.com>
2021-05-24 18:11:42 +0800

    Revert "not working"
    This reverts commit b5df49e04bbb46f68c6cad2f1be5fc45c1fdb4ce.

--- _helpers/send-email.js
+++ _helpers/send-email.js
@@ -1,10 +1,9 @@
 const nodemailer = require('nodemailer');
-
 const config = require('config.json');
+
 module.exports = sendEmail;
 
 async function sendEmail({ to, subject, html, from = config.emailFrom }) {
-    console.log("Params:"+`from: ${from}, To:${to} ,subject: ${subject} `);
     const transporter = nodemailer.createTransport(config.smtpOptions);
     await transporter.sendMail({ from, to, subject, html });
 }
